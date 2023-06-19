6bdf956d2285e1c52fc3f5337f0279574ea9a87d
tanishhky <studioustanishk@gmail.com>
2023-06-19 09:48:08 +0530

    h

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let songIdx=0;
 let audioEle = new Audio('Assets/CryBaby.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgBar=document.getElementById('myProgBar');
+let gif=document.getElementById('myProgBar');
 
 
 let songs=[
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Racing into the night",filePath:"Assets/.mp3",coverPath:"covers/1.jpg"},
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
@@ -15,17 +17,30 @@ let songs=[
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
     {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
-    {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
-    {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
+    // {songName:"CryBaby",filePath:"Assets/CryBaby.mp3",coverPath:"covers/1.jpg"},
 ]
 
 masterPlay.addEventListener('click',()=>{
     if(audioEle.paused || audioEle.currentTime<=0){
-        console.log("Entered this functions")
         audioEle.play();
+        masterPlay.classList.remove('fa-play');
+        masterPlay.classList.add('fa-pause');
+    }
+    else{
+        audioEle.pause();
+        masterPlay.classList.remove('fa-pause');
+        masterPlay.classList.add('fa-play');
     }
 })
 
-myProgBar.addEventListener('timeupdate',()=>{
+audioEle.addEventListener('timeupdate',()=>{
     console.log('timeupdate');
+    progress=parseInt((audioEle.currentTime/audioEle.duration)*100);
+    myProgBar.value=progress;
 })
+
+myProgBar.addEventListener('change',()=>{
+    // console.log("entered");
+    audioEle.currentTime=myProgBar.value * audioEle.duration/100;
+})
+
